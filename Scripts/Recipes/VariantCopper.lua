340ad96726f78d0bd0f0e2a13a05859097544196
Chloe-Bt <50842809+Chloe-Bt@users.noreply.github.com>
2025-05-05 11:12:23 +0200

    Update VariantCopper.lua

--- Scripts/Recipes/VariantCopper.lua
+++ Scripts/Recipes/VariantCopper.lua
@@ -14,7 +14,7 @@ data:extend{
         enabled = settings.startup["igrys-enable-all"].value,
         allow_productivity = true,
         energy_required = 3,
-        recipe_category = "electronics",
+        recipe_category = "electronics-or-assembling",
         allow_as_intermediate = false,
         auto_recycle = false
     }, {
